7c6441bd90bb2c2ed55871cd2756fff5bc7de29a
Anna Horodetska and Dan Jacob <dan@>
2017-03-29 14:06:42 -0700

    add executive consonant check and qu

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,28 @@ var pigLatin = function(input) {
    inputArr.push("ay");
    alert(inputArr);
 }
+  else if (inputArr[0] === "q" && inputArr[1] === "u"){
+    inputArr.splice(0,2);
+    inputArr.push("quay");
+    console.log(inputArr);
+  }
+
 else {
-  for (i=0; i<=inputArr.length; i++){
-    if (inputArr[i] !== "a" ||  inputArr[i] !== "e" || inputArr[i] !== "i" || inputArr[i] !== "o" ||  inputArr[i] !== "u"){
-      var consonant = inputArr[i];
-      console.log(consonant);
+  var consonant=[];
+  for (i=0; i<=inputArr.length;){
+    if (inputArr[i] === "a" ||  inputArr[i] === "e" || inputArr[i] === "i" || inputArr[i] === "o" ||  inputArr[i] === "u"){
+      break;
       }
+    else {
+      consonant.push(inputArr[i]);
+      inputArr.splice(i,1);
+    }
    }
+   var result = inputArr.concat(consonant);
+    result.push("ay");
+    console.log(result);
 }
+
 };
 
 $(document).ready(function(){

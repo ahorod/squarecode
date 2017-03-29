2b4732f74fdc76e356094e926c8d39d00c1f05ce
Anna Horodetska and Katie Borel <anna@>
2017-03-29 08:38:08 -0700

    add html, user interface js, flase condition for leapyear function

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var leapYear = function(year){
+  return false;
+};
+
+
+
+$(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});

8705f5c74098032130255a444347c06ffa4d3d4b
Anna Horodetska and Katie Borel <anna@>
2017-03-29 08:51:37 -0700

    add user input text check

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,13 @@ $(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+    $(".year").text(year);
+    if (!result) {
+      $(".not").text("not");
+    }
+    else {
+      $(".not").text("");
+    }
+    $("#result").show();
   });
 });

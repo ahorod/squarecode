aa75ad00f40e002dbc55be2aceed8637dcc29af5
Anna Horodetska and Brian Fan <anna@>
2017-03-30 10:06:55 -0700

    add initial html, specs, convert to roman numbers 1 through 10

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,47 @@
 
-var pigLatinWord = function(word) {
-  var result = [];
-  var inputArr = word.split ("");
-  if (inputArr[0] === "a" || inputArr[0] ==="e" ||inputArr[0] === "i" ||inputArr[0] === "o" ||inputArr[0] === "u" ){
-    console.log(inputArr[0]);
-   inputArr.push("ay");
-   inputArr = inputArr.join('');
-   result.push(inputArr);
-  //  var result = inputArr.join('');
-   console.log(result);
-  } else if (inputArr[0] === "q" && inputArr[1] === "u") {
-    inputArr.splice(0,2);
-    inputArr.push("quay");
-    inputArr = inputArr.join('');
-    result.push(inputArr);
-    console.log(result);
-  } else {
-    var consonant=[];
-    var test = inputArr;
-    var counter = 0;
-    for (var i=0; i < inputArr.length; i++){
-      if (inputArr[i] === "a" ||  inputArr[i] === "e" || inputArr[i] === "i" || inputArr[i] === "o" ||  inputArr[i] === "u"){
-        console.log(inputArr[i]);
-        break;
-        }
-      else {
-        consonant.push(inputArr[i]);
-        counter = counter + 1;
-     }
-   }
-
-     console.log(consonant, counter);
-     test.splice(0,counter)
-      var temp = test.concat(consonant);
-      temp.push("ay");
-      temp = temp.join('');
-      result.push(temp);
-      console.log(result);
+var convertToRoman = function(number) {
+  //Convert numbers from 1 to 10 to roman numerial
+  var number = parseInt(number);
+  var decimal = [1, 5, 10];
+  var roman = ["I", "V", "X"];
+  var romanString = " ";
+  var romanArr = [];
+  if (number < 4) {
+    for (i=0; i < number; i++){
+      romanString += "I";
+    }
   }
-  console.log(result);
-  return result;
-};
-
-var pigLatin = function(input){
-  var words = input.split(" ");
-  var sentenceResult = [];
-  for (i=0; i < words.length; i++){
-  var result = pigLatinWord(words[i]);
-  sentenceResult.push(result);
+  else if (number === 4) {
+    romanString += "IV";
   }
-  sentenceResult = sentenceResult.join(' ');
-  return sentenceResult;
+  else if (number === 5) {
+    romanString += "V";
+  }
+  else if (number >5 && number < 9) {
+    romanString = "V"
+    for (i=0; i < number-5; i++){
+      romanString += "I";
+    }
+  }
+  else if (number === 9) {
+    romanString += "IX";
+  }
+  else if (number === 10) {
+    romanString += "X";
+  }
+
+  console.log(romanString);
+
+
+
 }
 
-
 $(document).ready(function(){
-  $("form#pig-latin").submit(function(event) {
+  $("form#roman-number").submit(function(event) {
     event.preventDefault();
-    var input = $("#input").val();
-    var result = pigLatin(input);
-    $("#pig-result").text(result);
+    var number = $("#input").val();
+    var result = convertToRoman(number);
+    $("#convert-to-roman").text(result);
 
   });
 });

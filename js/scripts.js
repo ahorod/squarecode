6ef7c93752e7a0dabbd9eb7d2f8d84afec13ddde
Anna Horodetska and Brian Fan <brian@>
2017-03-30 16:41:11 -0700

    add split sentence into substrings

--- js/scripts.js
+++ js/scripts.js
@@ -1,153 +1,56 @@
 
-var convertSingleDigit = function(number) {
-  //Convert numbers from 1 to 10 to roman numerial
-  var number = parseInt(number);
-  var romanString = " ";
-
-  if (number < 4) {
-    for (i=0; i < number; i++){
-      romanString += "I";
-    }
-  } else if (number === 4) {
-    romanString += "IV";
-  } else if (number === 5) {
-    romanString += "V";
-  } else if (number >5 && number < 9) {
-    romanString = "V"
-    for (i=0; i < number-5; i++){
-      romanString += "I";
-    }
-  } else if (number === 9) {
-    romanString += "IX";
-
-  } else if (number === 10) {
-    romanString += "X";
-
-  }
-  console.log(romanString);
-  return romanString;
+var convertSquareCode = function(sentence) {
+  //removes panctuation from sentence
+sentence = sentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+sentence = sentence.replace(/ /g,'');
+// sentence = sentence.replace(' ','');
+console.log(sentence);
+sentence = sentence.toLowerCase();
+var length = sentence.length;
+var chunk = Math.sqrt(length);
+if (chunk%1 > 0){
+  var columns = Math.ceil(chunk);
+  var rows = Math.floor(chunk);
 }
-
-var convertToTens = function(number) {
-  //Convert numbers from 10 to 100 to roman numerial
-  var number = parseInt(number);
-  var romanStringTen = " ";
-  var tensInteger = Math.floor(number/10);
-  console.log(tensInteger);
-
-  if (tensInteger < 4) {
-    for (i=0; i < tensInteger; i++){
-      romanStringTen += "X";
-    }
-  } else if (tensInteger === 4) {
-    romanStringTen += "XL";
-  } else if (tensInteger === 5) {
-    romanStringTen += "L";
-  } else if (tensInteger >5 && tensInteger < 9) {
-    romanStringTen = "L"
-    for (i=0; i < tensInteger-5; i++){
-      romanStringTen += "X";
-    }
-  } else if (tensInteger === 9) {
-    romanStringTen += "XC";
-  } else if (tensInteger === 10) {
-    romanStringTen += "C";
-  }
-  console.log(romanStringTen);
-  if (number%10 > 0) {
-  var secondDigit = number%10;
-  var resultDigit = convertSingleDigit(secondDigit);
-  } else {
-    var resultDigit = "";
-  }
-
-  var resultTens = romanStringTen + resultDigit;
-  return resultTens;
-  console.log(resultTens);
+else {
+  columns = chunk;
+  rows = chunk;
 }
+// var sentenceArr = sentence.split('');
+console.log(sentence);
+// var newArray = [];
+// var i,j,temparray;
+// for (i=0,j=sentenceArr.length; i<j; i+=rows) {
+//     temparray = sentenceArr.slice(i,i+columns);
+//     newArray.push(temparray);
+//   console.log(temparray);
+// }
 
-var convertToHundreds = function(number) {
-  //Convert numbers from 10 to 100 to roman numerial
-  var number = parseInt(number);
-  var romanStringHundreds = " ";
-  var hundredsInteger = Math.floor(number/100);
-  console.log(hundredsInteger);
+sentence.match(/.{1, columns}/g);
+console.log(sentence);
 
-  if (hundredsInteger < 4) {
-    for (i=0; i < hundredsInteger; i++){
-      romanStringHundreds += "C";
-    }
-  } else if (hundredsInteger === 4) {
-    romanStringHundreds += "CD";
-  } else if (hundredsInteger === 5) {
-    romanStringHundreds += "D";
-  } else if (hundredsInteger >5 && hundredsInteger < 9) {
-    romanStringHundreds = "D"
-    for (i=0; i < hundredsInteger-5; i++){
-      romanStringHundreds += "C";
-    }
-  } else if (hundredsInteger === 9) {
-    romanStringHundreds += "CM";
-  } else if (hundredsInteger === 10) {
-    romanStringHundreds += "M";
-  }
-  console.log(romanStringHundreds);
 
-  if (number%100 > 0) {
-  var secondDigit = number%100;
-  var resultDigit = convertToTens(secondDigit);
-  } else {
-    var resultDigit = "";
-  }
+// for (var i=0; i < newArray.length; i++){
+//   for (var j=0; j < newArray[i].length; j++){
+//     string += newArray[i][j];
+//   }
+// }
 
-  var resultHundred = romanStringHundreds + resultDigit;
-  return resultHundred;
-  console.log(resultHundred);
+// for (var i=0; i < newArray[i].length; i++){
+//   for (var j=0; j < newArray.length; j++){
+//     string += newArray[i][j];
+//   }
+// }
+// console.log(newArray[0][0]);
+// console.log(string);
 }
-
-var convertToThousands = function(number) {
-  //Convert numbers from 100 to 3999 to roman numerial
-  var number = parseInt(number);
-  var romanStringThousands = " ";
-  var thousandsInteger = Math.floor(number/1000);
-  console.log(thousandsInteger);
-
-  if (thousandsInteger < 4) {
-    for (i=0; i < thousandsInteger; i++){
-      romanStringThousands += "M";
-    }
-  }
-
-  console.log(romanStringThousands);
-
-  if (number%1000 > 0) {
-  var secondDigit = number%1000;
-  var resultDigit = convertToHundreds(secondDigit);
-  } else {
-    var resultDigit = "";
-  }
-
-  var resultThousand= romanStringThousands + resultDigit;
-  // for(var j = 0; j < resultThousand.length; j++) {
-  //   if(resultThousand[j] === " ") {
-  //     resultThousand = resultThousand[j].replace(" ","");
-  //   }
-  // }
-  resultThousand = resultThousand.split(" ");
-  resultThousand = resultThousand.join("");
-
-  console.log(resultThousand);
-  return resultThousand;
-
-}
-
 
 $(document).ready(function(){
-  $("form#roman-number").submit(function(event) {
+  $("form#square-code").submit(function(event) {
     event.preventDefault();
-    var number = $("#input").val();
-    var result = convertToThousands(number);
-    $("#convert-to-roman").text(result);
+    var sentence = $("#input").val();
+    var result = convertSquareCode(sentence);
+    $("#cipher").text(result);
 
   });
 });

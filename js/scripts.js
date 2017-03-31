e2b92da1d9c53b6acb4aeb8aee605a57a9a3a787
ahorod <prozerpina112009@gmail.com>
2017-03-30 21:09:25 -0700

    add final output

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,51 @@
-
 var convertSquareCode = function(sentence) {
-  //removes panctuation from sentence
-sentence = sentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    //removes panctuation from sentence
+    sentence = sentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() \'\"]/g, "");
 
-sentence = sentence.replace(/ /g,'');
-console.log(sentence);
-sentence = sentence.toLowerCase();
-var length = sentence.length;
-var chunk = Math.sqrt(length);
-var columns, rows;
-if (chunk%1 > 0){
-  chunk = Math.floor(chunk);
-}
-else {
-  chunk = chunk;
-}
-var sentenceArr = sentence.split('');
-console.log(sentenceArr);
-var newArray = [];
+    console.log(sentence);
+    sentence = sentence.toLowerCase();
+    var length = sentence.length;
+    var chunk = Math.sqrt(length);
+    var columns, rows;
+    if (chunk % 1 > 0) {
+        chunk = Math.floor(chunk);
+    } else {
+        chunk = chunk;
+    }
+    var sentenceArr = sentence.split('');
+    console.log(sentenceArr);
+    var newArray = [];
 
-for (i=0; i < sentenceArr.length;i+=chunk) {
-    newArray = sentenceArr.slice(i,i+chunk);
+    for (i = 0; i < sentenceArr.length; i += chunk) {
+        newArray.push(sentenceArr.slice(i, i + chunk));
+    }
     console.log(newArray);
+    var encoded = [];
+    for (j = 0; j < chunk; j++) {
+        for (i = 0; i < newArray.length; i++) {
+            if (newArray[i][j] === undefined) {} else {
+                encoded.push(newArray[i][j]);
+            }
+        }
+    }
+    console.log(encoded);
+    var result = [];
+    for (i = 0; i < encoded.length; i += 5) {
+        result.push(encoded.slice(i, i + 5).join(''));
+    }
+    console.log(result);
+    var final;
+    final = result.join(' ')
+    console.log(final);
+    return final;
 }
 
-// for (var i=0; i < newArray.length; i++){
-//   for (var j=0; j < newArray[i].length; j++){
-//     string += newArray[i][j];
-//   }
-// }
+$(document).ready(function() {
+    $("form#square-code").submit(function(event) {
+        event.preventDefault();
+        var sentence = $("#input").val();
+        var result = convertSquareCode(sentence);
+        $("#cipher").text(result);
 
-// for (var i=0; i < newArray[i].length; i++){
-//   for (var j=0; j < newArray.length; j++){
-//     string += newArray[i][j];
-//   }
-// }
-// console.log(newArray[0][0]);
-// console.log(string);
-}
-
-$(document).ready(function(){
-  $("form#square-code").submit(function(event) {
-    event.preventDefault();
-    var sentence = $("#input").val();
-    var result = convertSquareCode(sentence);
-    $("#cipher").text(result);
-
-  });
+    });
 });
